f6e6904f3fea2b7b7267aa4ae27abb07c130d750
evan999 <emeshberge@gmail.com>
2021-02-27 17:00:05 -0500

    Building Gateway with RestTemplate.

---
+++ src/main/java/com/finance/Gateway/Models/CatalogItem.java
@@ -0,0 +1,27 @@
+package com.finance.Gateway.Models;
+
+public class CatalogItem {
+    private String name;
+    private String desc;
+    private int rating;
+
+    public CatalogItem(String name, String desc, int rating) {
+        this.name = name;
+        this.desc = desc;
+        this.rating = rating;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+
+}
---
+++ src/main/java/com/finance/Gateway/Resources/MovieCatalogResource.java
@@ -0,0 +1,21 @@
+package com.finance.Gateway.Resources;
+
+import com.finance.Gateway.Models.CatalogItem;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collections;
+import java.util.List;
+
+@RestController
+@RequestMapping("/catalog")
+public class MovieCatalogResource {
+
+    @RequestMapping("/{userId}")
+    public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
+        return Collections.singletonList(
+                new CatalogItem("Transformers", "Test", 4)
+        );
+    }
+}
