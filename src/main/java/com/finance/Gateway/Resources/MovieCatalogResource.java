c1902a3a602ec3fc892ee4a3392cb5ce68179050
evan999 <emeshberge@gmail.com>
2021-02-28 22:14:35 -0500

    Add in external API microservice call

--- src/main/java/com/finance/Gateway/Resources/MovieCatalogResource.java
+++ src/main/java/com/finance/Gateway/Resources/MovieCatalogResource.java
@@ -1,21 +1,38 @@
 package com.finance.Gateway.Resources;
 
 import com.finance.Gateway.Models.CatalogItem;
+import com.finance.Gateway.Models.Movie;
+import com.finance.Gateway.Models.Rating;
+import com.finance.Gateway.Models.UserRating;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/catalog")
 public class MovieCatalogResource {
 
+    @Autowired
+    private RestTemplate restTemplate;
+
     @RequestMapping("/{userId}")
     public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
-        return Collections.singletonList(
-                new CatalogItem("Transformers", "Test", 4)
-        );
+
+        RestTemplate restTemplate = new RestTemplate();
+
+        // get all rated movie IDs
+        UserRating ratings = restTemplate.getForObject("http://localhost:8082/ratingsdata/users" + userId, UserRating.class);
+
+        return ratings.getUserRating().stream().map(rating -> {
+            Movie movie = restTemplate.getForObject("http://localhost:8081/movies/" + rating.getMovieId(), Movie.class);
+            return new CatalogItem(movie.getName(), "Test", rating.getRating());
+        }).collect(Collectors.toList());
     }
 }
